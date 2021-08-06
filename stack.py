542996db8d12e097160a6831cf4d43328b8dc472
Mohammed ayman <mae252002@gmail.com>
2021-08-06 07:58:30 +0200

    Fix stack.py

--- stack.py
+++ stack.py
@@ -7,15 +7,6 @@ class LinkedList(object):
     def __init__(self, head=None):
         self.head = head
 
-    def append(self, new_element):
-        current = self.head
-        if self.head:
-            while current.next:
-                current = current.next
-            current.next = new_element
-        else:
-            self.head = new_element
-
     def insert_first(self, new_element):
         new_element.next = self.head
         self.head = new_element
